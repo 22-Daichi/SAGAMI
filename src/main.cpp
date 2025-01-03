f72297c1c43b9012675adeeced2413ac19573f0e
DESKTOP-J7UEU6D\daichi <tokyotechdai20@gmail.com>
2025-01-03 23:29:37 +0900

    あとはモーターだけ

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
+#include <Wire.h>
+
 const int rightMotor1 = 12;
 const int rightMotor2 = 13;
 const int leftMotor1 = 1;
@@ -12,33 +14,48 @@ const int activeBuzzer = 15;
 const int blueLed = 5;
 const int redLed = 16;
 
+const int outputPin[7] = {
+    rightMotor1,
+    rightMotor2,
+    leftMotor1,
+    leftMotor2,
+    activeBuzzer,
+    blueLed,
+    redLed};
+
 const int waterSensor = 4;
 
+int16_t ax, ay, az, gx, gy, gz, Temp;
+
 unsigned long lastTime = 0;
-unsigned long timerDelay = 1000; // send readings timer
+unsigned long timerDelay = 100; // send readings timer
 
 void gpioSetup()
 {
-    pinMode(rightMotor1, OUTPUT);
-    pinMode(rightMotor2, OUTPUT);
-    pinMode(leftMotor1, OUTPUT);
-    pinMode(leftMotor2, OUTPUT);
-    pinMode(activeBuzzer, OUTPUT);
-    pinMode(blueLed, OUTPUT);
-    pinMode(redLed, OUTPUT);
-    pinMode(waterSensor, INPUT);
+    for (int k = 0; k <= 6; k++)
+    {
+        pinMode(outputPin[k], OUTPUT);
+        digitalWrite(outputPin[k], 0);
+    }
 }
 
-void inputData()
+void getGy521Value()
 {
-    shipData.up = 1;
-    shipData.down = 1;
-    shipData.right = 1;
-    shipData.left = 1;
-    shipData.buzzer = 1;
-    shipData.temp = 1;
-    shipData.battery = 1;
-    shipData.water = 1;
+    // スタートアドレスの設定とデータの要求処理
+    Wire.beginTransmission(0x68);     // アドレス0x68指定でMPU-6050を選択、送信処理開始
+    Wire.write(0x3B);                 // ACCEL_XOUT_Hレジスタのアドレス指定
+    Wire.endTransmission(false);      // false設定で接続維持
+    Wire.requestFrom(0x68, 14, true); // MPU-6050に対して14byte分データを要求、I2Cバス開放
+    // シフト演算と論理和で16bitのデータを変数に格納
+    // ax～gzまで、16bit(2byte) × 7 = 14byte
+    ax = Wire.read() << 8 | Wire.read();      // x軸の加速度の読み取り 16bit
+    ay = Wire.read() << 8 | Wire.read();      // y軸の加速度の読み取り 16bit
+    az = Wire.read() << 8 | Wire.read();      // z軸の加速度の読み取り 16bit
+    Temp = Wire.read() << 8 | Wire.read();    // 温度の読み取り 16bit
+    Temp = int((Temp / 340.00 + 32.93) * 10); // 10倍の値として送信
+    gx = Wire.read() << 8 | Wire.read();      // x軸の角速度の読み取り 16bit
+    gy = Wire.read() << 8 | Wire.read();      // y軸の角速度の読み取り 16bit
+    gz = Wire.read() << 8 | Wire.read();      // z軸の角速度の読み取り 16bit
 }
 
 typedef struct struct_message
@@ -79,15 +96,41 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus)
         // Serial.println("Delivery fail");
         digitalWrite(redLed, 1);
         digitalWrite(blueLed, 0);
+        controllerData.up = 1;
+        controllerData.down = 1;
+        controllerData.right = 1;
+        controllerData.left = 1;
+        controllerData.buzzer = 1;
+    }
+}
+
+void inputData()
+{
+    shipData.up = 1;
+    shipData.down = 1;
+    shipData.right = 1;
+    shipData.left = 1;
+    shipData.buzzer = 1;
+    shipData.temp = Temp;
+    shipData.battery = int(analogRead(A0) - 30.25);
+    shipData.water = digitalRead(waterSensor);
+}
+
+void fireBuzzer()
+{
+    if (controllerData.buzzer == 0)
+    {
+        digitalWrite(activeBuzzer, 1);
+    }
+    else
+    {
+        digitalWrite(activeBuzzer, 0);
     }
 }
 
 void setup()
 {
-    // Serial.begin(9600);
-    // gpioSetup();
-    pinMode(redLed, OUTPUT);
-    pinMode(blueLed, OUTPUT);
+    gpioSetup();
     // Set device as a Wi-Fi Station
     WiFi.mode(WIFI_STA);
     if (esp_now_init() != 0)
@@ -99,16 +142,26 @@ void setup()
     esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
     esp_now_register_send_cb(OnDataSent);
     esp_now_register_recv_cb(OnDataRecv);
+
+    // GY-521用
+    Wire.begin(2, 14);            // I2C通信開始
+    Wire.beginTransmission(0x68); // アドレス0x68指定でMPU-6050を選択、送信処理開始
+    Wire.write(0x6B);             // MPU6050_PWR_MGMT_1レジスタのアドレス指定
+    Wire.write(0x00);             // 0を書き込むことでセンサ動作開始
+    Wire.endTransmission();       // スレーブデバイスに対する送信を完了
 }
 
 void loop()
 {
     if ((millis() - lastTime) > timerDelay)
     {
-        // digitalWrite(redLed, 1);
-        // Set values to send
-        // Send message via ESP-NOW
+        fireBuzzer();
+
+        // ここから送信用
+        getGy521Value();
+        inputData();
         esp_now_send(broadcastAddress, (uint8_t *)&shipData, sizeof(shipData));
+        // ここまで
         lastTime = millis(); // プログラム実行から経過した時間
     }
 }
